395dc2051ea8dcdd4080961e5a9dfb5d09c1baba
Adam Moore <adamaoc@gmail.com>
2013-11-08 16:33:54 -0500

    added theme files and setup work

---
+++ footer.php
@@ -0,0 +1,17 @@
+      </div>
+    </div>
+
+     <div id="footer">
+      <div class="container">
+        <p class="text-muted credit">
+          Example of the footer area in this project website. &copy all rights reserved.
+        </p>
+      </div>
+    </div>
+  
+    <script src="{{assets}}/js/jquery.js"></script>
+    <script src="{{assets}}/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Grunt Startup Project w/ Bootstrap3</title>
+
+    <meta name="description" content="UI-Less & Performant Transitions & Animations">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+
+    <!-- Individual module CSS files here -->
+    <!-- Should we combine or not combine? -->
+    <link rel="stylesheet" href="{{assets}}/css/style.css">
+</head>
+
+<body>
+
+  <!-- Wrap all page content here -->
+    <div id="wrap">
+    
+    <!-- Fixed navbar -->
+      <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">This Project name</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home2</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="page-header">
+          <h1>Grunt Startup Project w/ Bootstrap3</h1>
+        </div>
