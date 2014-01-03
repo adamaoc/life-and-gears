8e96e93725be82b3bcf4e88ca8077c45b69f2336
adamaoc <adamaoc@gmail.com>
2014-01-02 20:22:19 -0600

    added comments and changed up archive and single pages

--- single.php
+++ single.php
@@ -31,17 +31,15 @@
 				<!-- post details -->
 				<div class="post-details">
 					<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-					<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-					<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+					<span class="author"><?php _e( 'Published by', 'ampnetmedia' ); ?> <?php the_author_posts_link(); ?></span>
+					<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'ampnetmedia' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 				</div>
 				<!-- /post details -->
 				<?php the_content(); // Dynamic Content ?>
 			</section>
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+			<?php the_tags( __( 'Tags: ', 'ampnetmedia' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 			
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-			
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+			<p><?php _e( 'Categorised in: ', 'ampnetmedia' ); the_category(', '); // Separated by commas ?></p>
 			
 			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 			
@@ -67,6 +65,6 @@
 	</section>
 	<!-- /section -->
 	
-<?php get_sidebar(); ?>
+<?php // get_sidebar(); ?>
 
 <?php get_footer(); ?>
